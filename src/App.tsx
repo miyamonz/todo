9e921d2ad5469a4f7c1339fce662730c68b55e58
Koichi Miyamoto <m5813k@gmail.com>
2021-06-12 19:17:39 +0900

    show and add project task

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,13 @@ import { Layout } from "./Layout";
 
 import Home from "./Home";
 import Projects from "./Projects";
-
 import { Route } from "wouter";
+
+import { useUpdateAtom } from "jotai/utils";
+import { useFilterAtom } from "./jotaiUtils/filterAtom";
+import TaskList from "./Task/TaskList";
+import { tasksAtom } from "./store";
+import { newTask } from "./Task";
 
 function App() {
   return (
@@ -14,13 +19,29 @@ function App() {
         <Home />
       </Route>
       <Route path="/projects/:id">
-        <Home />
+        {({ id }) => <TaskListByProject id={id} />}
       </Route>
       <Route path="/projects">
         <Projects />
       </Route>
     </Layout>
   );
+}
+
+function useAddTask(id: string) {
+  const setTasks = useUpdateAtom(tasksAtom);
+  const addTask = React.useCallback(() => {
+    setTasks((prev) => [...prev, newTask({ projectId: id })]);
+  }, []);
+  return addTask;
+}
+
+function TaskListByProject({ id }: { id: string }) {
+  const filterToday = React.useCallback((t) => t.projectId === id, []);
+  const [filteredAtoms, remove] = useFilterAtom(tasksAtom, filterToday);
+  const addTask = useAddTask(id);
+
+  return <TaskList taskAtoms={filteredAtoms} remove={remove} add={addTask} />;
 }
 
 export default App;
--- src/Task/type.ts
+++ src/Task/type.ts
@@ -3,14 +3,19 @@ export interface Task {
   created: number; //unixtime
   updated: number; //unixtime
   done: boolean;
-  project?: string;
+  projectId?: string;
 }
-export function newTask(): Task {
+export interface TaskProp {
+  text?: string;
+  projectId?: string;
+}
+export function newTask(prop?: TaskProp): Task {
   const now = Math.floor(+new Date() / 1000);
   return {
     text: "",
     created: now,
     updated: now,
     done: false,
+    ...prop,
   };
 }
